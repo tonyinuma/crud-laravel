10eeb1e71c0a0df6d7d6054285fdfa97b3ce897a
Tony ID <tony.inuma@gmail.com>
2020-07-09 21:01:03 +0200

    add: Create model, resource and view Articulo

---
+++ app/Http/Controllers/ArticulosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Articulos;
+use Illuminate\Http\Request;
+
+class ArticulosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Articulos  $articulos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Articulos $articulos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Articulos  $articulos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Articulos $articulos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Articulos  $articulos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Articulos $articulos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Articulos  $articulos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Articulos $articulos)
+    {
+        //
+    }
+}
